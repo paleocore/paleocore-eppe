afd85c7749346e7935d08e516c143d652b3b3903
dennereed <denne.reed@gmail.com>
2021-07-13 13:50:01 -0700

    Added setup file

---
+++ __init__.py
@@ -0,0 +1,4 @@
+__license__ = 'MIT License <http://www.opensource.org/licenses/mit-license.php>'
+__author__ = 'Denne Reed <denne.reed@gmail.com>'
+__docformat__ = 'epytext'
+__version__ = '0.1.0'
---
+++ setup.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+
+from setuptools import setup, find_packages
+from . import __version__
+
+setup(
+	name='paleocore-eppe',
+	version=__version__,
+	author='Denne Reed',
+	author_email='denne.reed@gmail.com',
+	description='A Paleo Core app for managing Eyasi Plateau Paleontology Expedition (EPPE) data.',
+	url='https://github.com/paleocore/eppe',
+	packages=find_packages(),
+	include_package_data=True,
+	install_requires=('Django>=2.2.24', 'paleocore-lite>=0.1.0'),
+	zip_safe=False,
+	license='MIT',
+	classifiers=(
+		'Development Status :: 3 - Alpha',
+		'Environment :: Web Environment',
+		'Framework :: Django',
+		'Intended Audience :: Developers',
+		'Intended Audience :: Science/Research',
+		'License :: OSI Approved :: MIT License',
+		'Operating System :: OS Independent',
+		'Programming Language :: Python :: 3',
+		'Programming Language :: Python :: 3.9',
+	),
+)
